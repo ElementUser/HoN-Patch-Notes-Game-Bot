27e7e165dbea51393b2b2c2241a7c1f620ba12d4
William Lam <w.lam@honteam.com>
2020-08-07 23:02:22 -0400

    Target Reddit thread is now specified

--- patch_notes_game_bot/main.py
+++ patch_notes_game_bot/main.py
@@ -3,15 +3,11 @@ import praw
 
 def main():
     reddit = praw.Reddit('hon-bot', user_agent="HoN Patch Notes Game Bot by /u/hon-bot")
-
     subreddit = reddit.subreddit("HeroesofNewerth")
 
-    for submission in subreddit.hot(limit=5):
-        print("Title: ", submission.title)
-        print("Text: ", submission.selftext)
-        print("Score: ", submission.score)
-        print("---------------------------------\n")
-
+    patch_notes_url = "https://www.reddit.com/r/HeroesofNewerth/comments/huocgd/game_time_patch_version_485/"
+    patch_notes_thread = reddit.submission(url=patch_notes_url)
+    
 if __name__ == "__main__":
     main()
 
