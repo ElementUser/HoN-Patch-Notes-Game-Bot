428290def6d608a95a8f9cdc6cb883e32527c36d
William Lam <w.lam@honteam.com>
2020-08-07 23:08:34 -0400

    Iterating through top level comments only for now

--- patch_notes_game_bot/main.py
+++ patch_notes_game_bot/main.py
@@ -7,7 +7,12 @@ def main():
 
     patch_notes_url = "https://www.reddit.com/r/HeroesofNewerth/comments/huocgd/game_time_patch_version_485/"
     patch_notes_thread = reddit.submission(url=patch_notes_url)
-    
+
+    forest_comments = patch_notes_thread.comments
+
+    for comment in forest_comments:
+        print(comment.body)
+
 if __name__ == "__main__":
     main()
 
