9e82017165405d919c4e1001e604242f330bc777
William Lam <w.lam@honteam.com>
2020-08-09 21:50:55 -0400

    Moved input variables to top of file

--- patch_notes_game_bot/main.py
+++ patch_notes_game_bot/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import praw
 
+# Input variables for the program - change these when needed
+patch_notes_url = "https://www.reddit.com/r/HeroesofNewerth/comments/huocgd/game_time_patch_version_485/"
+
 
 def main():
     """
@@ -10,9 +13,9 @@ def main():
     reddit = praw.Reddit("hon-bot", user_agent="HoN Patch Notes Game Bot by /u/hon-bot")
     subreddit = reddit.subreddit("HeroesofNewerth")
 
-    patch_notes_url = "https://www.reddit.com/r/HeroesofNewerth/comments/huocgd/game_time_patch_version_485/"
     patch_notes_thread = reddit.submission(url=patch_notes_url)
 
+    # Get top level comments from the Reddit thread
     forest_comments = patch_notes_thread.comments
 
     for comment in forest_comments:
