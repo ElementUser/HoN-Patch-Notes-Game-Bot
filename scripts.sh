d3eb51091382a018477b1c70f56b0e18046f9a87
William Lam <w.lam@honteam.com>
2020-08-16 10:30:20 -0400

    Improved CICD Testing Report formatting with pytest-cov - Also added a helper scripts.sh file

---
+++ scripts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Author(s): William Lam
+# This .sh file helps map a sequence of commands/long commands to much shorter commands.
+
+if [[ $# -ne 1 ]]; then
+    echo "Invalid number of parameters. Running this script only requires 1 parameter."
+    exit 2
+fi
+
+case $1 in
+    "start")
+        cd hon_patch_notes_game_bot
+        poetry run python main.py
+        ;;
+
+    "test")
+        poetry run pytest --cov-report term-missing --cov=hon_patch_notes_game_bot tests/
+        ;;
+    *)
+        echo -e "Invalid option.\n"
+        echo "Current command list: "
+        echo "
+            start: runs the program after navigating to its source code directory (to ensure it runs properly with Poetry's venv)
+            test: runs a pytest command to output a test report with the help of the pytest-cov library
+        "
+        ;;
+esac
