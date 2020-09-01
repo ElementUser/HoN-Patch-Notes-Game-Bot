3f2b1b2c6fe2c9e8b4c0365654b1717a02921ec3
William Lam <w.lam@honteam.com>
2020-09-01 18:04:21 -0400

    Changed config for real test run

--- hon_patch_notes_game_bot/config/config.py
+++ hon_patch_notes_game_bot/config/config.py
@@ -5,12 +5,12 @@ This file will contain the bot script configuration that are most likely to chan
 # ==========
 # Variables
 # ==========
-submission_title = "[TEST v1] 4.8.5 - Patch Notes Guessing Game"
+submission_title = "[TEST RUN] 4.8.5 - Patch Notes Guessing Game"
 
 # ================
 # Other constants
 # ================
-SUBREDDIT_NAME = "testingground4bots"
+SUBREDDIT_NAME = "heroesofnewerth"
 MIN_COMMENT_KARMA = 5
 MAX_NUM_GUESSES = 2
 SLEEP_INTERVAL_SECONDS = 10
