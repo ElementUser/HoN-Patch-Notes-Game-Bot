f10328c93892c9ad0af0719dd5cde3ca9bf0cd83
William Lam <w.lam@honteam.com>
2021-03-11 21:08:40 -0500

    Update for 4.9.2 Patch Notes game

--- hon_patch_notes_game_bot/config/config.py
+++ hon_patch_notes_game_bot/config/config.py
@@ -7,22 +7,22 @@ This file will contain the bot script configuration that are most likely to chan
 # Variables
 # ==========
 
-# [GAME] 4.8.X - Patch Notes Guessing Game
-submission_title = "[GAME] 4.9.1 - Patch Notes Guessing Game"
-community_submission_title = "4.9.1 - Community-revealed Patch Notes"
-game_end_time = "January 28, 2021, 4:00 am UTC"
-gold_coin_reward = 300
+# [GAME] 4.9.X - Patch Notes Guessing Game
+submission_title = "[GAME] 4.9.2 - Patch Notes Guessing Game"
+community_submission_title = "4.9.2 - Community-revealed Patch Notes"
+game_end_time = "March 20, 2021, 4:00 am UTC"
+gold_coin_reward = 200
 
 # ================
 # Other constants
 # ================
 SUBREDDIT_NAME = "heroesofnewerth"  # testingground4bots / heroesofnewerth
-MAX_PERCENT_OF_LINES_REVEALED = 70  # Safety-oriented variable to ensure that the entire patch notes will not be revealed
+MAX_PERCENT_OF_LINES_REVEALED = 66  # Safety-oriented variable to ensure that the entire patch notes will not be revealed
 MAX_NUM_GUESSES = 2
 MIN_COMMENT_KARMA = 4
 MIN_LINK_KARMA = 4
 MIN_ACCOUNT_AGE_DAYS = 7
-NUM_WINNERS = 15
+NUM_WINNERS = 20
 SLEEP_INTERVAL_SECONDS = 10
 STAFF_MEMBER_THAT_HANDS_OUT_REWARDS = "FB-Saphirez"
 
