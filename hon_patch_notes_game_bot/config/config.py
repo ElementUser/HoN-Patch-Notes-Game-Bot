afc3b77047637d97da09fb707dcda98cded06af8
William Lam <w.lam@honteam.com>
2021-05-01 10:35:32 -0400

    Config changes for Patch 4.9.3

--- hon_patch_notes_game_bot/config/config.py
+++ hon_patch_notes_game_bot/config/config.py
@@ -2,16 +2,21 @@
 This file will contain the bot script configuration that are most likely to change
 """
 
-
 # ==========
 # Variables
 # ==========
 
 # [GAME] 4.9.X - Patch Notes Guessing Game
-submission_title = "[GAME] 4.9.2 - Patch Notes Guessing Game"
-community_submission_title = "4.9.2 - Community-revealed Patch Notes"
-game_end_time = "March 20, 2021, 4:00 am UTC"
-gold_coin_reward = 200
+submission_title = "[GAME] 4.9.3 - Patch Notes Guessing Game"
+community_submission_title = "4.9.3 - Community-revealed Patch Notes"
+game_end_time = "May 15, 2021, 4:00 am UTC"
+
+# ================
+# Reward settings
+# ================
+
+gold_coin_reward = 300
+NUM_WINNERS = 25
 
 # ================
 # Other constants
@@ -22,23 +27,25 @@ MAX_NUM_GUESSES = 2
 MIN_COMMENT_KARMA = 4
 MIN_LINK_KARMA = 4
 MIN_ACCOUNT_AGE_DAYS = 7
-NUM_WINNERS = 20
 SLEEP_INTERVAL_SECONDS = 10
 STAFF_MEMBER_THAT_HANDS_OUT_REWARDS = "FB-Saphirez"
 
 # ================
-# Lists
+# Data structures
 # ================
 
 # Disallowed users: use a set for the O(1) access time
 disallowed_users_set = {
+    "ElementUser",
+    "FB-Saphirez",
     "the_timezone_bot",
     "timee_bot",
     "generic_reddit_bot_2",
     "-koy",
     "-koy888",
     "rrreeddiitt",
-    "diabetis_"
+    "diabetis_",
+    "Apejohn",
 }
 
 # Recipients user list (for Private Messages) for the winners list
