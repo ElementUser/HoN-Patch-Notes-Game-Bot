7dbe4e40556eb4640b1b42f7ce643a1b81b6b954
William Lam <w.lam@honteam.com>
2021-05-04 19:26:23 -0400

    100% coverage for communications.send_message_to_staff()

--- tests/test_communications.py
+++ tests/test_communications.py
@@ -1,20 +1,33 @@
-from unittest import mock
+from unittest.mock import Mock, patch
+from praw.exceptions import RedditAPIException
+
 from hon_patch_notes_game_bot import communications
 from hon_patch_notes_game_bot.config.config import staff_recipients, gold_coin_reward
 
 
-@mock.patch("praw.Reddit")
-def test_send_message_to_staff(mock_reddit: mock.Mock):
-    mock_reddit.redditor = mock.Mock()
+@patch("praw.Reddit")
+def test_send_message_to_staff(mock_reddit: Mock):
+    # Inner test function that is called multiple times
+    def assert_test(mock_reddit):
+        assert (
+            communications.send_message_to_staff(
+                mock_reddit,
+                winners_list_path="./tests/cache/winners_list.txt",
+                staff_recipients=staff_recipients,
+                version_string="4.9.3",
+                gold_coin_reward=gold_coin_reward,
+            )
+            is None
+        )
+
+    mock_reddit.redditor = Mock()
     mock_reddit.redditor.message.return_value = None
 
-    assert (
-        communications.send_message_to_staff(
-            mock_reddit,
-            winners_list_path="./tests/cache/winners_list.txt",
-            staff_recipients=staff_recipients,
-            version_string="4.9.3",
-            gold_coin_reward=gold_coin_reward,
-        )
-        is None
-    )
+    # Regular use case
+    assert_test(mock_reddit)
+
+    # Exceptions
+    mock_reddit.redditor.message.side_effect = RedditAPIException
+    assert_test(mock_reddit)
+    mock_reddit.redditor.message.side_effect = ValueError
+    assert_test(mock_reddit)
