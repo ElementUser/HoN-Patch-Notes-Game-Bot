fbe261bfcc78e2a1fc3980b07a0ceaa8b7a31bc4
William Lam <w.lam@honteam.com>
2021-05-05 13:44:00 -0400

    Initial testing for test_core

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,4 +1,27 @@
-from hon_patch_notes_game_bot import core
+from unittest.mock import patch, Mock
+from unittest import TestCase
+from pytest import mark
 
-# This file currently has no tests,
-#   as the 'core' module largely relies on API calls from other libraries (e.g. praw).
+from hon_patch_notes_game_bot import core
+from hon_patch_notes_game_bot.database import Database
+from tests.test_patch_notes_file_handler import patch_notes_file
+from tests.test_database import database_path
+
+
+class TestCore(TestCase):
+    @mark.usefixtures("patch_notes_file")
+    def setup_class(self):
+        self.mock_reddit = patch("praw.Reddit")
+        self.mock_submission = patch("praw.models.Submission")
+        self.mock_community_submission = patch("praw.models.Submission")
+        self.database = Database(db_path=database_path)
+        self.core = core.Core(
+            reddit=self.mock_reddit,
+            db=self.database,
+            submission=self.mock_submission,
+            community_submission=self.mock_community_submission,
+            patch_notes_file=patch_notes_file,
+        )
+
+    def test_empty(self):
+        pass
