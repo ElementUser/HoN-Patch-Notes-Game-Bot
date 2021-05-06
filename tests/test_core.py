bacd91813b25632f1578cf3fea33f8e2d30300d7
William Lam <w.lam@honteam.com>
2021-05-06 11:55:45 -0400

    Resolved test_update_patch_notes_table_in_db

--- tests/test_core.py
+++ tests/test_core.py
@@ -18,12 +18,16 @@ class TestCore(TestCase):
         self.mock_reddit = patch("praw.Reddit")
         self.mock_submission = patch("praw.models.Submission")
         self.mock_community_submission = patch("praw.models.Submission")
+        self.mock_author = patch("praw.models.Redditor")
+        self.mock_comment = patch("praw.models.Comment")
+        self.dummy_user = RedditUser("User1")
 
         # Configure mocks
         self.mock_submission.selftext = "Test string"
         self.mock_submission.edit = Mock()
         self.mock_community_submission.selftext = "Test string"
         self.mock_community_submission.edit = Mock()
+        self.mock_author.name = "User2"
 
         # Create a concrete Core object
         self.core = core.Core(
@@ -41,61 +45,52 @@ class TestCore(TestCase):
         def assert_test():
             assert (
                 self.core.reply_with_bad_guess_feedback(
-                    mock_user, mock_author, mock_comment, first_line
+                    self.dummy_user, self.mock_author, self.mock_comment, first_line
                 )
                 is None
             )
 
-        mock_user = RedditUser("User1")
-        mock_author = patch("praw.models.Redditor")
-        mock_author.name = "User2"
-        mock_comment = patch("praw.models.Comment")
         first_line = ""
 
         # Regular case
         assert_test()
 
         # Cannot guess case
-        mock_user.can_submit_guess = False
+        self.dummy_user.can_submit_guess = False
         assert_test()
 
         # User is potential winner
-        mock_user.is_potential_winner = True
+        self.dummy_user.is_potential_winner = True
         assert_test()
 
     def test_get_user_from_database(self):
-        mock_author = patch("praw.models.Redditor")
-
         # Existing user
-        mock_author.name = "Tehnubzor"
-        assert self.core.get_user_from_database(mock_author)
+        self.mock_author.name = "Tehnubzor"
+        assert self.core.get_user_from_database(self.mock_author)
 
         # Non-existing user
-        mock_author.name = "I_do_not_exist_asjdoiajsdiodwjowjdwq"
-        assert self.core.get_user_from_database(mock_author)
+        self.mock_author.name = "I_do_not_exist_asjdoiajsdiodwjowjdwq"
+        assert self.core.get_user_from_database(self.mock_author)
 
     def test_is_account_too_new(self):
-        mock_author = patch("praw.models.Redditor")
-
         # "Old" user (passes the check)
-        mock_author.created_utc = 1609390800  # December 31, 2020 at 00:00:00
-        assert not self.core.is_account_too_new(mock_author, MIN_ACCOUNT_AGE_DAYS)
+        self.mock_author.created_utc = 1609390800  # December 31, 2020 at 00:00:00
+        assert not self.core.is_account_too_new(self.mock_author, MIN_ACCOUNT_AGE_DAYS)
 
         # "New" user (fails the check)
-        mock_author.created_utc = datetime.utcnow().timestamp()
-        assert self.core.is_account_too_new(mock_author, MIN_ACCOUNT_AGE_DAYS)
+        self.mock_author.created_utc = datetime.utcnow().timestamp()
+        assert self.core.is_account_too_new(self.mock_author, MIN_ACCOUNT_AGE_DAYS)
 
     def test_safe_comment_reply(self):
         # Regular use case
-        mock_comment = patch("praw.models.Comment")
-        assert self.core.safe_comment_reply(mock_comment, "Test Body") is None
+        assert self.core.safe_comment_reply(self.mock_comment, "Test Body") is None
 
         # Exceptions
-        mock_comment.reply = Mock()
-        mock_comment.reply.side_effect = RedditAPIException(
+        self.mock_comment.reply = Mock()
+        self.mock_comment.reply.side_effect = RedditAPIException(
             ["RATELIMIT", "Error message", None], "Optional string",
         )
-        assert self.core.safe_comment_reply(mock_comment, "Test Body") is None
+        assert self.core.safe_comment_reply(self.mock_comment, "Test Body") is None
 
     def test_update_community_compiled_patch_notes_in_submission(self):
         assert (
@@ -106,36 +101,55 @@ class TestCore(TestCase):
         )
 
     def test_is_disallowed_to_post(self):
-        mock_redditor = patch("praw.models.Redditor")
-        mock_comment = patch("praw.models.Comment")
-
         # Disallowed users set condition
-        mock_redditor.name = "ElementUser"
-        assert self.core.is_disallowed_to_post(mock_redditor, mock_comment)
-        mock_redditor.name = "Some_username"
+        self.mock_author.name = "ElementUser"
+        assert self.core.is_disallowed_to_post(self.mock_author, self.mock_comment)
+        self.mock_author.name = "Some_username"
 
         # No verified email
-        mock_redditor.has_verified_email = False
-        assert self.core.is_disallowed_to_post(mock_redditor, mock_comment)
-        mock_redditor.has_verified_email = True
+        self.mock_author.has_verified_email = False
+        assert self.core.is_disallowed_to_post(self.mock_author, self.mock_comment)
+        self.mock_author.has_verified_email = True
 
         # Comment karma is 0
-        mock_redditor.comment_karma = 0
-        mock_redditor.link_karma = 0
-        assert self.core.is_disallowed_to_post(mock_redditor, mock_comment)
-        mock_redditor.comment_karma = 9999
-        mock_redditor.link_karma = 9999
+        self.mock_author.comment_karma = 0
+        self.mock_author.link_karma = 0
+        assert self.core.is_disallowed_to_post(self.mock_author, self.mock_comment)
+        self.mock_author.comment_karma = 9999
+        self.mock_author.link_karma = 9999
 
         # is_account_too_new() condition is True
-        mock_redditor.created_utc = datetime.utcnow().timestamp()
-        assert self.core.is_disallowed_to_post(mock_redditor, mock_comment)
-        mock_redditor.created_utc = 1609390800  # December 31, 2020 at 00:00:00
+        self.mock_author.created_utc = datetime.utcnow().timestamp()
+        assert self.core.is_disallowed_to_post(self.mock_author, self.mock_comment)
+        self.mock_author.created_utc = 1609390800  # December 31, 2020 at 00:00:00
 
         # Function should return False now
-        assert not self.core.is_disallowed_to_post(mock_redditor, mock_comment)
+        assert not self.core.is_disallowed_to_post(self.mock_author, self.mock_comment)
 
     def test_fix_corrupted_community_submission_edit(self):
         assert self.core.fix_corrupted_community_submission_edit() is None
 
     def test_perform_post_game_actions(self):
         assert self.core.perform_post_game_actions() is None
+
+    def test_update_patch_notes_table_in_db(self):
+        patch_notes_line_number = 1
+
+        assert self.core.update_patch_notes_table_in_db(
+            self.dummy_user,
+            self.mock_author,
+            self.mock_comment,
+            patch_notes_line_number,
+        )
+
+        # If patch notes line number is already guessed
+        patch(
+            "hon_patch_notes_game_bot.database.Database.check_patch_notes_line_number",
+            return_value=True,
+        )
+        assert not self.core.update_patch_notes_table_in_db(
+            self.dummy_user,
+            self.mock_author,
+            self.mock_comment,
+            patch_notes_line_number,
+        )
