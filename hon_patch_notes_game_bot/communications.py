077e30e460de803151893354f7046d64bb30ff39
William Lam <wlam063@uottawa.ca>
2021-05-11 09:09:42 -0400

    Release Version 0.2.2 (#8)
    Release Version 0.2.2
    - Performed a large code structure refactor for massively improved unit testing coverage.
    - Unit testing coverage increased from ~40% to 92%.
    - Fixed a few minor edge cases that unit testing has found while performing the refactor.
    
    Unit Testing details
    - Mocks are now used where appropriate to "mimic" outputs from the Reddit API (via PRAW).
    - Fixtures are now used for some repetitive setup/teardown operations (particularly with the database)
    - Fixtures can now be imported from conftest.py
    - Database state is now properly preserved between test runs
    
    Other minor details for changes
    - VSCode settings.json file has been updated for the Python Testing Framework VSCode plugin
    - All constants were moved from main.py to config/config.py
    - main.py now has considerably less logic in it (it only contains instantiation logic)
    - core.py now processes most of the game rule logic, broken down into different functions for testability purposes.

--- hon_patch_notes_game_bot/communications.py
+++ hon_patch_notes_game_bot/communications.py
@@ -1,13 +1,85 @@
+#!/usr/bin/python
 """
 This module contains functions related to communications across the Reddit platform
 """
 import time
 import re
-from typing import List
+from typing import List, Tuple
 
-from praw.exceptions import RedditAPIException
 from praw import Reddit
-from hon_patch_notes_game_bot.config.config import STAFF_MEMBER_THAT_HANDS_OUT_REWARDS
+from praw.exceptions import RedditAPIException
+from praw.models import Subreddit, Submission
+from hon_patch_notes_game_bot.database import Database
+from hon_patch_notes_game_bot.patch_notes_file_handler import PatchNotesFile
+from hon_patch_notes_game_bot.utils import (
+    processed_submission_content,
+    processed_community_notes_thread_submission_content,
+)
+from hon_patch_notes_game_bot.config.config import (
+    COMMUNITY_SUBMISSION_TITLE,
+    STAFF_MEMBER_THAT_HANDS_OUT_REWARDS,
+    SUBMISSION_TITLE,
+)
+
+
+def init_submissions(
+    reddit: Reddit,
+    subreddit: Subreddit,
+    database: Database,
+    patch_notes_file: PatchNotesFile,
+    submission_content_path: str,
+    community_submission_content_path: str,
+) -> Tuple[Submission, Submission]:
+    """
+    Initializes the primary and community submission (i.e. "Reddit threads") objects.
+    If they do not exist in the database, then this function creates them.
+        Otherwise, it retrieves the submissions via their URL from the database.
+
+    Returns:
+        - A tuple containing the primary submission and community submission objects
+    """
+    # Main submission
+    submission_content = processed_submission_content(
+        submission_content_path, patch_notes_file
+    )
+    submission: Submission = None
+    submission_url = database.get_submission_url(tag="main")
+
+    # Get main submission if it does not exist
+    if submission_url is None:
+        submission = subreddit.submit(
+            title=SUBMISSION_TITLE, selftext=submission_content
+        )
+        database.insert_submission_url("main", submission.url)
+        submission_url = submission.url
+    else:
+        # Obtain submission via URL
+        submission = reddit.submission(url=submission_url)
+
+    # Community submission
+    community_submission_content = processed_community_notes_thread_submission_content(
+        community_submission_content_path, patch_notes_file, submission_url
+    )
+    community_submission: Submission = None
+    community_submission_url = database.get_submission_url(tag="community")
+
+    # Get community submission if it does not exist
+    if community_submission_url is None:
+        community_submission = subreddit.submit(
+            title=COMMUNITY_SUBMISSION_TITLE, selftext=community_submission_content,
+        )
+        database.insert_submission_url("community", community_submission.url)
+
+        # Update main Reddit Thread's in-line URL to connect to the community submission URL
+        updated_text = submission.selftext.replace(
+            "#community-patch-notes-thread-url", community_submission.url
+        )
+        submission.edit(body=updated_text)
+    else:
+        # Obtain submission via URL
+        community_submission = reddit.submission(url=community_submission_url)
+
+    return submission, community_submission
 
 
 def send_message_to_staff(
@@ -44,13 +116,15 @@ def send_message_to_staff(
                     subject=subject_line, message=winners_list_text
                 )
             except RedditAPIException as redditError:
+                print(f"RedditAPIException encountered: {redditError}")
                 print(
-                    f"{redditError}\n{recipient} was not sent a message, continuing to next recipient"
+                    f"{recipient} was not sent a message, continuing to next recipient"
                 )
                 continue
             except Exception as error:
+                print(f"General Exception encountered: {error}")
                 print(
-                    f"{error}\n{recipient} was not sent a message, continuing to next recipient"
+                    f"{recipient} was not sent a message, continuing to next recipient"
                 )
                 continue
 
@@ -62,6 +136,16 @@ def send_message_to_winners(
     Sends the winners list results to a list of recipients via Private Message (PM).
 
     This function uses recursion to send messages to failed recipients.
+
+    This function also frequently encounters Reddit API Exceptions due to rate limits.
+        To sleep for the appropriate duration without wasting time, the rate limit error is parsed:
+
+    Test strings for regex capture:
+    RATELIMIT: "Looks like you've been doing that a lot. Take a break for 4 minutes before trying again." on field 'ratelimit'
+    RATELIMIT: "Looks like you've been doing that a lot. Take a break for 47 seconds before trying again." on field 'ratelimit'
+    RATELIMIT: "Looks like you've been doing that a lot. Take a break for 4 minutes 47 seconds before trying again."
+        on field 'ratelimit'
+    RATELIMIT: "Looks like you've been doing that a lot. Take a break for 1 minute before trying again." on field 'ratelimit'
 
     Attributes:
         reddit: the PRAW Reddit instance
@@ -80,6 +164,7 @@ def send_message_to_winners(
             f"You have been chosen by the bot as a winner for the {version_string} Patch Notes Guessing Game!\n\n"
             f"Please send /u/{STAFF_MEMBER_THAT_HANDS_OUT_REWARDS} a Private Message (PM) to request a code"
             f" for {str(gold_coin_reward)} Gold Coins.\n\n"
+            "Be sure to check your Reddit Chat inbox as well (not just your Reddit mail inbox)!\n\n"
             "Thank you for participating in the game! =)"
         )
         try:
@@ -88,30 +173,40 @@ def send_message_to_winners(
 
         # Reddit API Exception
         except RedditAPIException as redditException:
-            failed_recipients_list.append(recipient)
-            print(
-                f"{redditException}\n{recipient} was not sent a message (added to retry list), continuing to next recipient"
-            )
+            print(f"Full Reddit Exception: {redditException}\n\n")
 
-            # Rate limit error handling
-            if redditException.error_type == "RATELIMIT":
-                # Error printouts for debugging
-                print(f"Full Reddit Exception: {redditException}\n\n")
-                print(f"Reddit Exception Item 0: {redditException.items[0]}\n\n")
+            for subException in redditException.items:
+                # Rate limit error handling
+                if subException.error_type == "RATELIMIT":
+                    failed_recipients_list.append(recipient)
+                    print(
+                        f"{redditException}\n{recipient} was not sent a message (added to retry list), "
+                        "continuing to next recipient"
+                    )
+                    print(f"Subexception: {subException}\n\n")
 
-                # Sleep for the rate limit duration by parsing the minute count from exception message
-                regex_capture = re.search(
-                    r"(\d+) minutes", redditException.items[0].message
-                )
+                    # Sleep for the rate limit duration by parsing the minute and seconds count from
+                    #   the message into named groups
+                    regex_capture = re.search(
+                        r"\s+((?P<minutes>\d+) minutes?)?\s?((?P<seconds>\d+) seconds)?\s+",
+                        subException.message,
+                    )
+                    if regex_capture is None:
+                        print("Invalid regex detected. Sleeping for 60 seconds...")
+                        time.sleep(60)
+                        break
+                    else:
+                        # Use named groups from regex capture and assign them to a dictionary
+                        sleep_time_regex_groups = regex_capture.groupdict(default=0)
+                        secondsToSleep = int(
+                            sleep_time_regex_groups.get("minutes")  # type: ignore
+                        ) + int(
+                            sleep_time_regex_groups.get("seconds")  # type: ignore
+                        )  # type: ignore
 
-                if regex_capture is None:
-                    print("Invalid regex detected. Sleeping for 60 seconds...")
-                    time.sleep(60)
-                else:
-                    minutesToSleep = regex_capture.group(1)
-                    secondsToSleep = int(minutesToSleep) * 60
-                    print("Sleeping for " + str(secondsToSleep) + " seconds")
-                    time.sleep(secondsToSleep)
+                        print(f"Sleeping for {str(secondsToSleep)} seconds")
+                        time.sleep(secondsToSleep)
+                        break
 
             continue
 
@@ -123,7 +218,10 @@ def send_message_to_winners(
 
     # At the end of the function, recurse this function to re-send messages to failed recipients
     # Recurse only if failed_recipients_list has content in it
-    if len(failed_recipients_list) > 0:
+    # Prevents infinite loops by ensuring that the failed recipients count
+    #   gradually progresses towards the end condition.
+    failed_recipients = len(failed_recipients_list)
+    if failed_recipients > 0 and failed_recipients < len(winners_list):
         send_message_to_winners(
             reddit, failed_recipients_list, version_string, gold_coin_reward
         )
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,125 +1,126 @@
+import os
+import shutil
 import pytest
-import copy
+from pytest import mark
+
 from hon_patch_notes_game_bot.database import Database
 from hon_patch_notes_game_bot.user import RedditUser
 from tinydb import Query
 
-database_path = "./tests/cache/db_test.json"
+
+@pytest.fixture(scope="class")
+def setup_and_teardown_test_database(request):
+    # Setup
+    database_path = "./tests/cache/db_test.json"
+    database_backup_path = "./tests/cache/db_test_backup.json"
+
+    # Store backup database file to be restored later
+    shutil.copy2(database_path, database_backup_path)
+    request.cls._database = Database(db_path=database_path)
+
+    # Perform tests in the class that uses this fixture
+    yield
+
+    # Teardown
+    shutil.copy2(database_backup_path, database_path)
+    os.remove(database_backup_path)
 
 
-@pytest.fixture
-def database():
-    """Initializes patch notes file object"""
-    return Database(db_path=database_path)
+@mark.usefixtures("setup_and_teardown_test_database")
+class TestDataBase:
+    def test_insert_submission_url(self):
+        url_string = "http://test_url.com"
+        self._database.insert_submission_url(
+            tag="test_entry", submission_url=url_string
+        )
+        assert self._database.db.table("submission").search(Query().url == url_string)
+        # Remove entry from database afterwards
+        self._database.db.table("submission").remove(Query().url == url_string)
 
+    def test_get_submission_url(self):
+        url_string = "https://www.reddit.com/r/HeroesofNewerth/comments/in14hz/game_486_patch_notes_guessing_game/"
+        assert self._database.get_submission_url(tag="main") == url_string
 
-# ============
-# Unit tests
-# ============
-def test_insert_submission_url(database):
-    url_string = "http://test_url.com"
-    database.insert_submission_url(tag="test_entry", submission_url=url_string)
-    assert database.db.table("submission").search(Query().url == url_string)
-    # Remove entry from database afterwards
-    database.db.table("submission").remove(Query().url == url_string)
+    def test_get_unfound_submission_url(self):
+        assert self._database.get_submission_url(tag="unfound_url_tag") is None
 
+    def test_user_exists(self):
+        assert self._database.user_exists("S2Sliferjam")
+        assert not self._database.user_exists(
+            "random_user_that_doesnt_exist_89213u893u13u132u139u31983u13dasdadsadsa"
+        )
 
-def test_get_submission_url(database):
-    url_string = "https://www.reddit.com/r/HeroesofNewerth/comments/in14hz/game_486_patch_notes_guessing_game/"
-    assert database.get_submission_url(tag="main") == url_string
+    def test_get_user(self):
+        assert self._database.get_user("S2Sliferjam")
 
+    def test_add_user(self):
+        username = "random_user_that_temporarily_exists_213123asd"
+        user = RedditUser(name=username)
+        self._database.add_user(user)
+        assert self._database.user_exists(username)
 
-def test_user_exists(database):
-    assert database.user_exists("S2Sliferjam")
-    assert not database.user_exists(
-        "random_user_that_doesnt_exist_89213u893u13u132u139u31983u13dasdadsadsa"
-    )
+    def test_convert_db_user_to_RedditUser(self):
+        db_user = self._database.get_user("S2Sliferjam")
+        reddit_user = self._database.convert_db_user_to_RedditUser(db_user)
 
+        # Cannot iterate over an object's attributes directly,
+        # but can make a dictionary of their attributes via vars()
+        reddit_user_attributes = vars(reddit_user)
 
-def test_get_user(database):
-    assert database.get_user("S2Sliferjam")
+        for key in db_user:
+            assert db_user[key] == reddit_user_attributes[key]
 
+    def test_update_user(self):
+        updated_num_guesses = 1000
+        db_user = self._database.get_user("S2Sliferjam")
+        reddit_user = self._database.convert_db_user_to_RedditUser(db_user)
 
-def test_add_user(database):
-    username = "random_user_that_temporarily_exists_213123asd"
-    user = RedditUser(name=username)
-    database.add_user(user)
-    assert database.user_exists(username)
-    # Remove entry from database afterwards
-    database.db.table("user").remove(Query().name == username)
+        # Change the num_guesses field & test that
+        reddit_user.num_guesses = updated_num_guesses
+        self._database.update_user(reddit_user)
+        new_db_user = self._database.get_user("S2Sliferjam")
+        assert new_db_user["num_guesses"] == updated_num_guesses
 
+    def test_check_patch_notes_line_number(self):
+        assert self._database.check_patch_notes_line_number(69)
 
-def test_convert_db_user_to_RedditUser(database):
-    db_user = database.get_user("S2Sliferjam")
-    reddit_user = database.convert_db_user_to_RedditUser(db_user)
+    def test_check_unfound_patch_notes_line_number(self):
+        assert not self._database.check_patch_notes_line_number(69420)
 
-    # Cannot iterate over an object's attributes directly,
-    # but can make a dictionary of their attributes via vars()
-    reddit_user_attributes = vars(reddit_user)
+    def test_add_patch_notes_line_number(self):
+        added_line_number = 77777
+        self._database.add_patch_notes_line_number(added_line_number)
+        assert self._database.check_patch_notes_line_number(added_line_number)
+        self._database.db.table("patch_notes_line_tracker").remove(
+            Query().id == added_line_number
+        )
 
-    for key in db_user:
-        assert db_user[key] == reddit_user_attributes[key]
+    def test_get_entry_count_in_patch_notes_line_tracker(self):
+        entry_count = self._database.get_entry_count_in_patch_notes_line_tracker()
 
+        ## We know the number of lines in the test database
+        assert entry_count == 102
 
-def test_update_user(database):
-    updated_num_guesses = 1000
-    db_user = database.get_user("S2Sliferjam")
-    reddit_user = database.convert_db_user_to_RedditUser(db_user)
+    def test_get_potential_winners_list(self):
+        potential_winners_list = self._database.get_potential_winners_list()
+        assert len(potential_winners_list) > 0
 
-    # Save original user data
-    original_reddit_user = copy.deepcopy(reddit_user)
+    def test_get_random_winners_from_list(self):
+        num_winners = 10
+        potential_winners_list = self._database.get_potential_winners_list()
+        random_winners_list = self._database.get_random_winners_from_list(
+            num_winners=num_winners, potential_winners_list=potential_winners_list,
+        )
 
-    # Change the num_guesses field & test that
-    reddit_user.num_guesses = updated_num_guesses
-    database.update_user(reddit_user)
-    new_db_user = database.get_user("S2Sliferjam")
-    assert new_db_user["num_guesses"] == updated_num_guesses
+        assert len(random_winners_list) == 10
 
-    # Reupdate user in database with original user data once the test is complete
-    database.update_user(original_reddit_user)
+        # Check for unique values in list. Reference: https://stackoverflow.com/a/5278167
+        assert len(set(random_winners_list)) == len(random_winners_list)
 
-
-def test_check_patch_notes_line_number(database):
-    assert database.check_patch_notes_line_number(69)
-
-
-def test_add_patch_notes_line_number(database):
-    added_line_number = 77777
-    database.add_patch_notes_line_number(added_line_number)
-    assert database.check_patch_notes_line_number(added_line_number)
-    database.db.table("patch_notes_line_tracker").remove(
-        Query().id == added_line_number
-    )
-
-
-def test_get_entry_count_in_patch_notes_line_tracker(database):
-    entry_count = database.get_entry_count_in_patch_notes_line_tracker()
-
-    ## We know the number of lines in the test database
-    assert entry_count == 102
-
-
-def test_get_potential_winners_list(database):
-    potential_winners_list = database.get_potential_winners_list()
-    assert len(potential_winners_list) > 0
-
-
-def test_get_random_winners_from_list(database):
-    num_winners = 10
-    potential_winners_list = database.get_potential_winners_list()
-    random_winners_list = database.get_random_winners_from_list(
-        num_winners=num_winners, potential_winners_list=potential_winners_list,
-    )
-
-    assert len(random_winners_list) == 10
-
-    # Check for unique values in list. Reference: https://stackoverflow.com/a/5278167
-    assert len(set(random_winners_list)) == len(random_winners_list)
-
-    overly_large_num_winners = 7777777
-    random_winners_list = database.get_random_winners_from_list(
-        num_winners=overly_large_num_winners,
-        potential_winners_list=potential_winners_list,
-    )
-    assert len(random_winners_list) < overly_large_num_winners
+        overly_large_num_winners = 7777777
+        random_winners_list = self._database.get_random_winners_from_list(
+            num_winners=overly_large_num_winners,
+            potential_winners_list=potential_winners_list,
+        )
+        assert len(random_winners_list) < overly_large_num_winners
 
